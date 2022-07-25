d4040b17ba9c765edfc099b0081d10735e0454e7
victorrufino21 <victorrufino21@gmail.com>
2022-07-25 02:59:30 -0300

    Alteração

---
+++ protifolio/dashboard-responsivo/orders.js
@@ -0,0 +1,50 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        ProductNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pendeing'
+    },
+
+    {
+        productName: 'LARVENDER KF102 Drone',
+        ProductNumber: '36378',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+
+    {
+        productName: 'Ruko F11 Pro Drone',
+        ProductNumber: '49347',
+        paymentStatus: 'Due',
+        shipping: 'Pendeing'
+    },
+
+    {
+        productName: 'Drone With Camera Drone',
+        ProductNumber: '96996',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+
+    {
+        productName: 'GPS 4K Drone',
+        ProductNumber: '22821',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+
+    {
+        productName: 'DJI Air 2S',
+        ProductNumber: '81475',
+        paymentStatus: 'Due',
+        shipping: 'Pendeing'
+    },
+
+    {
+        productName: 'Lozenge Drone',
+        ProductNumber: '00482',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+]
